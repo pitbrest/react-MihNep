72d24916ef38d4185bd812482799c478af1a7252
pitbrest <sasha.petrov@tut.by>
2023-02-27 22:57:38 +0300

    sc

--- train/src/App.jsx
+++ train/src/App.jsx
@@ -3,30 +3,30 @@ import './App.css';
 
 class App extends Component {
 	state = {
-		timer: 0
-	};
-
-	stopTimer = () => {
-		clearInterval(this.myInterval);
+		timer: 0,
+		status: false
 	};
 
 	startTimer = () => {
+		this.setState({ status: true });
 		this.myInterval = setInterval(() => {
-			this.setState(prev => {
-				return {
-					timer: prev.timer + 1
-				};
-			});
+			this.setState({ timer: this.state.timer + 1 });
 		}, 1000);
+	};
+
+	stopTimer = () => {
+		this.setState({ status: false });
+		clearInterval(this.myInterval);
+	};
+
+	resetTimer = () => {
+		clearInterval(this.myInterval);
+		this.setState({ timer: 0, status: false });
 	};
 
 	componentDidMount() {
 		if (localStorage.timerValue) {
 			this.setState({ timer: +localStorage.timerValue });
-			this.startTimer();
-		} else {
-			this.setState({ timer: 0 });
-			this.startTimer();
 		}
 	}
 
@@ -42,8 +42,8 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<div className="nav">
-					<button onClick={this.stopTimer}>stop</button>
-					<button onClick={this.startTimer}>start</button>
+					<button onClick={this.state.status ? this.stopTimer : this.startTimer}>{this.state.status ? 'stop' : 'start'}</button>
+					<button onClick={this.resetTimer}>reset</button>
 				</div>
 				<div className="timer">{this.state.timer}</div>
 			</div>
