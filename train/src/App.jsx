80d63ea68f845878f2704b44fd3e9db1a8043a43
pitbrest <sasha.petrov@tut.by>
2023-03-01 00:20:12 +0300

    sc

--- train/src/App.jsx
+++ train/src/App.jsx
@@ -1,51 +1,56 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
 	state = {
-		timer: 0,
-		status: false
+		card: '',
+		email: '',
+		checkedStatus: false
 	};
 
-	startTimer = () => {
-		this.setState({ status: true });
-		this.myInterval = setInterval(() => {
-			this.setState({ timer: this.state.timer + 1 });
-		}, 1000);
+	cardnumberRef = React.createRef();
+	emailRef = React.createRef();
+
+	cardNumberHandler = (e) => {
+		this.setState(() => {
+			return { card: e.target.value };
+		}, () => { if (this.state.card.length === 6) { this.emailRef.current.focus(); } });
 	};
 
-	stopTimer = () => {
-		this.setState({ status: false });
-		clearInterval(this.myInterval);
+	emailHandler = (e) => {
+		this.setState({ email: e.target.value });
 	};
 
-	resetTimer = () => {
-		clearInterval(this.myInterval);
-		this.setState({ timer: 0, status: false });
+	checkboxHandler = (e) => {
+		this.setState({
+			checkedStatus: e.target.checked
+		});
 	};
 
 	componentDidMount() {
-		if (localStorage.timerValue) {
-			this.setState({ timer: +localStorage.timerValue });
-		}
+		this.cardnumberRef.current.focus();
 	}
 
-	componentWillUnmount() {
-		clearInterval(this.myInterval);
-	}
+	checking = () => {
+		const isValidEmail = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/.test(this.state.email);
+		return isValidEmail ? alert('all is ok') : alert('need to do something');
+	};
 
 	render() {
-		window.onbeforeunload = () => {
-			localStorage.timerValue = this.state.timer;
-		};
-
 		return (
 			<div className='App'>
-				<div className="nav">
-					<button onClick={this.state.status ? this.stopTimer : this.startTimer}>{this.state.status ? 'stop' : 'start'}</button>
-					<button onClick={this.resetTimer}>reset</button>
+				<div className="content">
+					<div className="info">
+						<input type="number" placeholder='card number' ref={this.cardnumberRef} value={this.state.card} onChange={this.cardNumberHandler} />
+						<input type="email" placeholder='e-mail' ref={this.emailRef} value={this.state.email} onChange={this.emailHandler} />
+					</div>
+					<label >
+						<input type="checkbox" checked={this.state.checkedStatus} onChange={this.checkboxHandler} />
+						check
+					</label>
+					<button onClick={this.checking}>Отправить</button>
 				</div>
-				<div className="timer">{this.state.timer}</div>
+
 			</div>
 
 		);
