c9444dd36124d5f13187d56d0e1d36557bb13319
pitbrest <sasha.petrov@tut.by>
2023-03-13 23:25:59 +0300

    sc

--- train/src/components/SearchPanel/SearchPanel.jsx
+++ train/src/components/SearchPanel/SearchPanel.jsx
@@ -20,22 +20,26 @@ class SearchPanel extends Component {
           <input id="search" type="text" className="validate" placeholder='search' style={{ letterSpacing: '1.8px' }}
             value={this.state.searchValue} onChange={this.inputHandler} />
 
-
-          <form action="!#">  <label>
-            <input class="with-gap" name="group3" type="radio" checked />
-            <span>Red</span>
-          </label>
-
-            <label>
-              <input class="with-gap" name="group3" type="radio" />
-              <span>Red</span>
-            </label>
-
-            <label>
-              <input class="with-gap" name="group3" type="radio" />
-              <span>Red</span>
-            </label></form>
-
+          <form action="#">
+            <p>
+              <label>
+                <input class="with-gap" name="group1" type="radio" defaultChecked />
+                <span>All</span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input class="with-gap" name="group1" type="radio" />
+                <span>Movies only</span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input class="with-gap" name="group1" type="radio" />
+                <span>Series only</span>
+              </label>
+            </p>
+          </form>
 
         </div>
       </div>
