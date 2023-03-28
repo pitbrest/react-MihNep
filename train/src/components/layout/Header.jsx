cf11a3c2fb831b8425d6e201c6605283b5ce07c0
pitbrest <sasha.petrov@tut.by>
2023-03-28 21:10:43 +0300

    sc

--- train/src/components/layout/Header.jsx
+++ train/src/components/layout/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 function Header() {
-  return (
-    <nav>
-      <div className="nav-wrapper teal">
-        <a href="!#" className="brand-logo">React Movies</a>
-      </div>
-    </nav>
-  );
+	console.log('go');
+	return (
+		<nav>
+			<div className='nav-wrapper teal'>
+				<a
+					href='!#'
+					className='brand-logo'>
+					React Movies
+				</a>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
