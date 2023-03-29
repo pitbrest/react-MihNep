276c94a619c9e64ca08a6cc01adbcdba23e6de0d
pitbrest <sasha.petrov@tut.by>
2023-03-30 00:22:52 +0300

    sc

--- train/src/api/OMDbAPI.jsx
+++ train/src/api/OMDbAPI.jsx
@@ -1,14 +1,11 @@
-const responseTemplate = 'http://www.omdbapi.com/?apikey=f5431db0&s=';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const responseTemplate = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`;
 
 const searchByTitle = async function (title, type = '') {
 	try {
 		const response = await fetch(responseTemplate + title + '&type=' + type);
 		const result = await response.json();
 		return result.Search;
-		// if (result.Search) {
-		// 	return result.Search;
-		// }
-		// return alert(result.Error);
 	} catch (error) {
 		console.error(
 			error.message,
