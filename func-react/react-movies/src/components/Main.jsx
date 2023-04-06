13e24fb336173b21fb2fa72b432fddf0ad4b421b
pitbrest <sasha.petrov@tut.by>
2023-04-06 23:54:34 +0300

    sc

--- func-react/react-movies/src/components/Main.jsx
+++ func-react/react-movies/src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SearchPanel from './search-panel/Search-panel';
 
 function Main() {
-	return <main className='container'>Main</main>;
+	return (
+		<main className='container'>
+			<SearchPanel />
+		</main>
+	);
 }
 
 export default Main;
--- func-react/react-movies/src/components/search-panel/Search-panel.jsx
+++ func-react/react-movies/src/components/search-panel/Search-panel.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import './Search-panel.css';
 
 function SearchPanel() {
-	return <div>Search-panel</div>;
+	return (
+		<div className='search-container'>
+			<div className='search-block pt-5 mb-3 d-flex gap-3'>
+				<input
+					type='text'
+					placeholder='enter your request'
+					className='form-control py-2 fw-medium'
+				/>
+				<button className='btn btn-success'>search</button>
+			</div>
+			<form className='filter-block d-flex gap-5 ps-2'>
+				<div class='form-check'>
+					<input
+						class='form-check-input'
+						type='radio'
+						name='flexRadioDefault'
+						id='flexRadioDefault1'
+					/>
+					<label
+						class='form-check-label'
+						for='flexRadioDefault1'>
+						All
+					</label>
+				</div>
+				<div class='form-check'>
+					<input
+						class='form-check-input'
+						type='radio'
+						name='flexRadioDefault'
+						id='flexRadioDefault1'
+					/>
+					<label
+						class='form-check-label'
+						for='flexRadioDefault1'>
+						Movies only
+					</label>
+				</div>
+				<div class='form-check'>
+					<input
+						class='form-check-input'
+						type='radio'
+						name='flexRadioDefault'
+						id='flexRadioDefault1'
+					/>
+					<label
+						class='form-check-label'
+						for='flexRadioDefault1'>
+						Series only
+					</label>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default SearchPanel;
