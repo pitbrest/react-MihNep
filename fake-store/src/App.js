0a526827b9e1b777d5ea0f359d1f3553a07fa940
pitbrest <sasha.petrov@tut.by>
2023-04-20 00:10:47 +0300

    start fake-store APP

---
+++ fake-store/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+	return (
+		<div className='App text-center'>
+			<Button variant='primary'>hello</Button>
+		</div>
+	);
+}
+
+export default App;
